8209685f86c58688e524a3ef774f3caca9fe83b3
Sarfraz-64 <130149008+Sarfraz-64@users.noreply.github.com>
2025-05-13 11:37:58 +0500

    Add Summary Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './Styles/App.css';
-import ButtonAppBar from './Components/ButtonAppBar'
-import Projects from './Components/Projects'
-import Education from './Components/Education'
-import Contact from './Components/Contact'
+import Summary from './Components/Summary';
+import ButtonAppBar from './Components/ButtonAppBar';
+import Projects from './Components/Projects';
+import Education from './Components/Education';
+import Contact from './Components/Contact';
+
 function App() {
   return (
     <>
-    <ButtonAppBar/>
-    <h2>Education:</h2>
-    <Education/>
-    <h2>Projects:</h2>
-    <Projects/>
-    <h2>Contact:</h2> 
-    <Contact/>
+      <ButtonAppBar />
+       <div id="Summary">
+        <h2>Summary</h2>
+        <Summary/>
+      </div>
+      <div id="education">
+        <h2>Education</h2>
+        <Education />
+      </div>
+      <div id="projects">
+        <h2>Projects</h2>
+        <Projects />
+      </div>
+      <div id="contact">
+        <h2>Contact</h2>
+        <Contact />
+      </div>
     </>
   );
 }
---
+++ src/Components/Summary.js
@@ -0,0 +1,12 @@
+function Summary(){
+    return(
+        <p style={{ textAlign: 'justified', margin: '0 auto', maxWidth: '1000px' }}>Enthusiastic and detail-oriented Computer Science student seeking an
+opportunity in a field-related company to apply my technical skills and
+academic knowledge. Passionate about problem-solving, software
+development, and emerging technologies, with a strong desire to
+contribute to innovative projects while gaining hands-on experience in
+the tech industry.
+</p>
+    );
+}
+export default Summary;
